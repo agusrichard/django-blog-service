014737547ca7b2afe882e7c0a0e216337770abbb
Agus Richard <agus.richard21@gmail.com>
2020-11-04 23:32:19 +0700

    template register and login endpoints

--- blog/authentication/views.py
+++ blog/authentication/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# Index page
 def index(request):
-    print('request', request)
     return HttpResponse('Authentication index endpoint')
+
+# Register
+def register(request):
+    return HttpResponse('Register user')
+
+# Login
+def login(request):
+    return HttpResponse('Login user')
