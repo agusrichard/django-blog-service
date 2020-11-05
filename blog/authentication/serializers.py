23ba023c6e7e731869f1456e4a3eff38fcc69ce6
Agus Richard <agus.richard21@gmail.com>
2020-11-05 23:24:35 +0700

    Create user serializer

---
+++ blog/authentication/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from .models import User
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(max_length=254)
+    password = serializers.CharField(max_length=128)
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        return instance
