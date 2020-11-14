7ac10bff885c961414bb6949af70941997274501
Agus Richard <agus.richard21@gmail.com>
2020-11-14 23:40:22 +0700

    User model

--- blog/users/models.py
+++ blog/users/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+
+from .managers import CustomUserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # ADMIN = 1
+    # MODERATOR = 2
+
+    # ROLE_CHOICES = (
+
+    # )
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    uid = models.UUIDField(unique=True, editable=False, default=uuid.uuid4, verbose_name='Public Identifier')
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100, blank=True)
+    # role = 
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    created_by = models.EmailField()
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_by = models.EmailField()
+    modified_date = models.DateTimeField(default=timezone.now)
+
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
