04e1d2bb3964d9ab01be16a5259829ed4bc019db
Agus Richard <agus.richard21@gmail.com>
2020-12-06 20:51:58 +0700

    Sekret

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o86sp$s%%^z@wgip!!z1**v2wavc5ju)pik)h)p((qx$p=m%f3'
+SECRET_KEY = 'sekret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
