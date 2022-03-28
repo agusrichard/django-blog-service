2da47aa55c65ed2513df65c5076e0dbfbb99df94
Agus Richard Lubis <agus.richard21@gmail.com>
2022-03-28 19:06:08 +0700

    Add user to post

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Post(models.Model):
@@ -6,6 +7,9 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        get_user_model(), db_column="user_id", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
