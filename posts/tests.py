9b6e113972f2dbf40d374985529e8d66e3b6a02b
Agus Richard Lubis <agus.richard21@gmail.com>
2022-03-29 21:41:07 +0700

    Update tests on posts app

--- posts/tests.py
+++ posts/tests.py
@@ -1,21 +1,28 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 from posts.models import Post
 
 
 class PostTests(TestCase):
     def test_create_post(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_superuser(
+            "email@example.com", "user_name", "password"
+        )
         post = Post.objects.create(
-            title="Test title",
-            content="Test content",
+            title="Test title", content="Test content", author=user
         )
         self.assertEqual(post.title, "Test title")
         self.assertEqual(post.content, "Test content")
 
     def test_create_retrieve(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_superuser(
+            "email@example.com", "user_name", "password"
+        )
         post = Post.objects.create(
-            title="Test title",
-            content="Test content",
+            title="Test title", content="Test content", author=user
         )
         self.assertEqual(post.title, "Test title")
         self.assertEqual(post.content, "Test content")
@@ -24,9 +31,12 @@ class PostTests(TestCase):
         self.assertEqual(post.content, "Test content")
 
     def test_create_update_post(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_superuser(
+            "email@example.com", "user_name", "password"
+        )
         post = Post.objects.create(
-            title="Test title",
-            content="Test content",
+            title="Test title", content="Test content", author=user
         )
         self.assertEqual(post.title, "Test title")
         self.assertEqual(post.content, "Test content")
@@ -36,9 +46,12 @@ class PostTests(TestCase):
         self.assertEqual(post.content, "New content")
 
     def test_create_delete_post(self):
+        user_model = get_user_model()
+        user = user_model.objects.create_superuser(
+            "email@example.com", "user_name", "password"
+        )
         post = Post.objects.create(
-            title="Test title",
-            content="Test content",
+            title="Test title", content="Test content", author=user
         )
         self.assertEqual(post.title, "Test title")
         self.assertEqual(post.content, "Test content")
