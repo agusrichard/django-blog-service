eab145b59e57f2746592fdb49b1d2cd4f58fe5dc
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-01 20:10:13 +0700

    More tests

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from django.test import TestCase
 from rest_framework import status
+from django.db import IntegrityError
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 
@@ -11,14 +12,9 @@ class UserTests(TestCase):
         new_superuser = db.objects.create_superuser(
             "email@example.com", "user_name", "password"
         )
-        self.assertEqual(new_superuser.email, "email@example.com")
-        self.assertEqual(new_superuser.user_name, "user_name")
-        self.assertEqual(str(new_superuser), "<User: email@example.com>")
-        self.assertTrue(new_superuser.is_active)
-        self.assertTrue(new_superuser.is_staff)
-        self.assertTrue(new_superuser.is_superuser)
+        self.assert_superuser_content(new_superuser)
 
-        with self.assertRaises(ValueError) as err:
+        with self.assertRaises(ValueError):
             db.objects.create_superuser(
                 email="email@example.com",
                 user_name="user_name",
@@ -45,26 +41,82 @@ class UserTests(TestCase):
     def test_new_user(self):
         db = get_user_model()
         new_user = db.objects.create_user("email@example.com", "user_name", "password")
-        self.assertEqual(new_user.email, "email@example.com")
-        self.assertEqual(new_user.user_name, "user_name")
-        self.assertEqual(str(new_user), "<User: email@example.com>")
-        self.assertFalse(new_user.is_active)
-        self.assertFalse(new_user.is_staff)
-        self.assertFalse(new_user.is_superuser)
+        new_user.save()
+        self.assert_user_content(new_user)
 
         with self.assertRaises(ValueError):
             db.objects.create_user("", "user_name3", "password")
 
+    def assert_superuser_content(self, user):
+        self.assertEqual(user.email, "email@example.com")
+        self.assertEqual(user.user_name, "user_name")
+        self.assertEqual(str(user), "<User: email@example.com>")
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
+
+    def assert_user_content(self, user):
+        self.assertEqual(user.email, "email@example.com")
+        self.assertEqual(user.user_name, "user_name")
+        self.assertEqual(str(user), "<User: email@example.com>")
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_user_notfound(self):
+        db = get_user_model()
+        with self.assertRaises(db.DoesNotExist):
+            db.objects.get(email="email@example.com")
+
+    def test_new_user_same_email(self):
+        db = get_user_model()
+        db.objects.create_user("email@example.com", "user_name1", "password1")
+        with self.assertRaises(IntegrityError):
+            db.objects.create_user("email@example.com", "user_name2", "password2")
+
+    def test_new_user_same_user_name(self):
+        db = get_user_model()
+        db.objects.create_user("email1@example.com", "user_name1", "password1")
+        with self.assertRaises(IntegrityError):
+            db.objects.create_user("email2@example.com", "user_name1", "password2")
+
+    def test_update_user(self):
+        db = get_user_model()
+        user = db.objects.create_user("email@example.com", "user_name", "password")
+        self.assert_user_content(user)
+
+        user.first_name = "first_name"
+        user.last_name = "last_name"
+        user.bio = "bio"
+        user.save()
+
+        self.assertEqual(user.first_name, "first_name")
+        self.assertEqual(user.last_name, "last_name")
+        self.assertEqual(user.bio, "bio")
+
+    def test_delete_user(self):
+        db = get_user_model()
+        user = db.objects.create_user("email@example.com", "user_name", "password")
+        user.save()
+
+        user.delete()
+        with self.assertRaises(db.DoesNotExist):
+            db.objects.get(email="email@example.com")
+
 
 class UserAPITests(APITestCase):
-    def test_user_api(self):
+    def create_user(self):
         url = reverse("users:create_user")
         data = {
             "email": "test_user@example.com",
             "user_name": "test_user",
             "password": "test_user",
         }
-        response = self.client.post(url, data, format="json")
+
+        return self.client.post(url, data, format="json"), data
+
+    def test_create_user_api(self):
+        response, _ = self.create_user()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get("email"), "test_user@example.com")
         self.assertEqual(response.data.get("user_name"), "test_user")
@@ -73,3 +125,44 @@ class UserAPITests(APITestCase):
         self.assertEqual(response.data.get("bio"), "")
         self.assertFalse(response.data.get("is_active"))
         self.assertFalse(response.data.get("is_staff"))
+
+    def test_activate_user_api(self):
+        response_create, data = self.create_user()
+        self.assertEqual(response_create.status_code, status.HTTP_201_CREATED)
+
+        url = reverse("users:activate_user")
+        response_activate = self.client.post(url, data, format="json")
+        self.assertEqual(response_activate.status_code, status.HTTP_200_OK)
+
+    def test_login_user_api(self):
+        response_create, data = self.create_user()
+        self.assertEqual(response_create.status_code, status.HTTP_201_CREATED)
+
+        url = reverse("users:activate_user")
+        response_activate = self.client.post(url, data, format="json")
+        self.assertEqual(response_activate.status_code, status.HTTP_200_OK)
+
+        url = reverse("users:token_obtain_pair")
+        response_login = self.client.post(url, data, format="json")
+        self.assertEqual(response_login.status_code, status.HTTP_200_OK)
+        self.assertTrue("access" in response_login.data)
+        self.assertTrue("refresh" in response_login.data)
+
+    def test_get_profile_api(self):
+        response_create, data = self.create_user()
+        self.assertEqual(response_create.status_code, status.HTTP_201_CREATED)
+
+        url = reverse("users:activate_user")
+        response_activate = self.client.post(url, data, format="json")
+        self.assertEqual(response_activate.status_code, status.HTTP_200_OK)
+
+        url = reverse("users:token_obtain_pair")
+        response_login = self.client.post(url, data, format="json")
+        self.assertEqual(response_login.status_code, status.HTTP_200_OK)
+
+        url = reverse("users:retrieve_user")
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Bearer " + response_login.data["access"]
+        )
+        response_profile = self.client.get(url)
+        self.assertEqual(response_profile.status_code, status.HTTP_200_OK)
