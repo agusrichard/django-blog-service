fd0e90277e2b756df2b487bcce46780def565f35
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-05 23:01:29 +0700

    I guess its working now for the Comment models

--- posts/models.py
+++ posts/models.py
@@ -27,3 +27,26 @@ class Post(models.Model):
 
     def get_likes(self):
         return self.likes.all()
+
+    def add_comment(self, user, content):
+        Comment.objects.create(user=user, post=self, content=content)
+
+    def get_comments(self):
+        return self.post_comments.all()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="user_comments"
+    )
+    post = models.ForeignKey(
+        "posts.Post", on_delete=models.CASCADE, related_name="post_comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post} - {self.content}"
+
+    class Meta:
+        ordering = ["-created_at"]
