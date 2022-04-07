4c7a42dc21c91ca882def793fb070941fe70f2e6
Agus Richard Lubis <agus.richard21@gmail.com>
2022-04-07 19:42:22 +0700

    New tests for likes app

--- likes/tests.py
+++ likes/tests.py
@@ -1,22 +1,74 @@
+from django.urls import reverse
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 from users.models import User
 from posts.models import Post
 
+CONST_TITLE = "Test title"
+CONST_CONTENT = "Test content"
+CONST_EMAIL = "email@example.com"
+CONST_USERNAME = "user_name"
+CONST_PASSWORD = "password"
+
 
 class LikeTests(TestCase):
-    def test_create_like(self):
-        new_user = User.objects.create_user(
+    def setUp(self):
+        self._user = User.objects.create_user(
             "email@example.com", "user_name", "password"
         )
-        post = Post.objects.create(
-            title="Test title", content="Test content", author=new_user
+
+        self._post = Post.objects.create(
+            title="Test title", content="Test content", author=self._user
         )
-        post.like(new_user)
-        self.assertEqual(post.likes.count(), 1)
-        return new_user, post
+
+    def test_create_like(self):
+        self._post.like(self._user)
+        self.assertNotEqual(self._post.likes.count(), 0)
 
     def test_create_delete_like(self):
-        user, post = self.test_create_like()
-        post.unlike(user)
-        self.assertEqual(post.likes.count(), 0)
+        self.test_create_like()
+        self._post.unlike(self._user)
+        self.assertEqual(self._post.likes.count(), 0)
+
+
+class LikeAPITests(APITestCase):
+    def setUp(self):
+        url = reverse("users:create_user")
+        data = {
+            "email": CONST_EMAIL,
+            "user_name": CONST_USERNAME,
+            "password": CONST_PASSWORD,
+        }
+
+        self.client.post(url, data, format="json")
+
+        url = reverse("users:activate_user")
+        self.client.post(url, data, format="json")
+
+        url = reverse("users:token_obtain_pair")
+        response = self.client.post(url, data, format="json")
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + response.data["access"])
+
+        url = reverse("posts:list_create_posts")
+        data = {"title": "Title", "content": "Content"}
+        self.client.post(url, data, format="json")
+
+    def test_like_post(self):
+        url = reverse("likes:like_post", kwargs={"post_id": 1})
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_unlike_post(self):
+        self.test_like_post()
+        url = reverse("likes:unlike_post", kwargs={"post_id": 1})
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_get_likes_post(self):
+        self.test_like_post()
+        url = reverse("likes:get_likes_post", kwargs={"post_id": 1})
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
--- likes/urls.py
+++ likes/urls.py
@@ -7,5 +7,5 @@ app_name = "likes"
 urlpatterns = [
     path("like_post/<int:post_id>/", like_post, name="like_post"),
     path("unlike_post/<int:post_id>/", unlike_post, name="unlike_post"),
-    path("get_likes/<int:post_id>/", get_likes, name="get_likes"),
+    path("get_likes/<int:post_id>/", get_likes, name="get_likes_post"),
 ]
